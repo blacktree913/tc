bacc1219d0e7b489cce3a8dbdaa840b6af5cb770
blacktree913 <1399527103@qq.com>
2020-06-15 16:15:08 +0800

    Initial commit

---
+++ src/main/resources/static/js/add.js
@@ -0,0 +1,6 @@
+// 新增按钮
+function add() {
+    // 打开新增框架
+    document.getElementById('addBlock').style.display = 'block';
+    document.getElementById('totalBackground').style.display = 'block';
+}
